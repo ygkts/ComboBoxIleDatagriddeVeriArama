f8c34380fac33b59f7ac1e43c39b83a4f2cf9f62
Yasemin Göktaş <ygkts61@gmail.com>
2019-01-24 18:24:49 +0300

    ilk

---
+++ y1_ComboboxtanDatagriddeArama/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace y1_ComboboxtanDatagriddeArama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxCateName = new System.Windows.Forms.ComboBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(495, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(475, 467);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(25, 109);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(435, 399);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Category Name :";
+            // 
+            // comboBoxCateName
+            // 
+            this.comboBoxCateName.FormattingEnabled = true;
+            this.comboBoxCateName.Location = new System.Drawing.Point(162, 41);
+            this.comboBoxCateName.Name = "comboBoxCateName";
+            this.comboBoxCateName.Size = new System.Drawing.Size(264, 24);
+            this.comboBoxCateName.TabIndex = 3;
+            this.comboBoxCateName.SelectedIndexChanged += new System.EventHandler(this.comboBoxCateName_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ProductName";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "UnitPrice";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "UnitsInStock";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "CategoryName";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "CategoryName";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ProductName";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "UnitPrice";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "UnitsInStock";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 542);
+            this.Controls.Add(this.comboBoxCateName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxCateName;
+    }
+}
+
---
+++ y1_ComboboxtanDatagriddeArama/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace y1_ComboboxtanDatagriddeArama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Server = YASEMINGOKTAS;  Database = NORTHWND; Trusted_Connection=true;");
+        SqlCommand cmd;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboDoldur();
+        }
+        private void ComboDoldur()
+        {
+            cmd = new SqlCommand("Select CategoryID, CategoryName from Categories", conn);
+            if (conn.State == ConnectionState.Closed)   // eğer connection kapalıysa aç.
+            {
+                conn.Open();
+            }
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())   // okuyacak bir şey kalmayana kadar verileri okuyarak 1 veya 0 döndürür. okuyacak bir şey yoksa 0 döner.
+            {
+                comboBoxCateName.Items.Add(dr[1].ToString());  // categoryname'leri combobox a dolduruyoruz.
+            }
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        private void comboBoxCateName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UrunListele();
+            DataGridUrunListele();
+        }
+        private void UrunListele()
+        {
+            cmd = new SqlCommand("Select c.CategoryName, p.ProductName, p.UnitPrice, p.UnitsInStock From Products p inner join Categories c on c.CategoryID = p.CategoryID where c.CategoryName = @CatName",conn);
+            cmd.Parameters.AddWithValue("@CatName", comboBoxCateName.SelectedItem.ToString());
+            // cmd.Parameters.AddWithValue("@CatName", comboBoxCateName.Text);
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataReader dr = cmd.ExecuteReader();
+            listView1.Items.Clear();
+
+            while (dr.Read())
+            {
+                ListViewItem kayit = new ListViewItem();
+                kayit.Text = dr["CategoryName"].ToString(); // veya->  kayit.Text = dr[0].ToString();
+                kayit.SubItems.Add(dr["ProductName"].ToString());
+                kayit.SubItems.Add(dr["UnitPrice"].ToString());
+                kayit.SubItems.Add(dr["UnitsInStock"].ToString());
+
+                listView1.Items.Add(kayit);
+
+             }
+
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        private void DataGridUrunListele()
+        {
+            cmd = new SqlCommand("Select c.CategoryName, p.ProductName, p.UnitPrice, p.UnitsInStock From Products p inner join Categories c on c.CategoryID = p.CategoryID where c.CategoryName = @CatName", conn);
+            cmd.Parameters.AddWithValue("@CatName", comboBoxCateName.SelectedItem.ToString());
+            // cmd.Parameters.AddWithValue("@CatName", comboBoxCateName.Text);
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataReader dr = cmd.ExecuteReader();
+            int satir = 0;
+            dataGridView1.Rows.Clear();
+
+            while (dr.Read())
+            {
+                satir = dataGridView1.Rows.Add();   // add otomatik olarak row eklediği için satir'i 1 arttırmaya gerek kalmıyor. add() int değer döndürür.
+                // satir ++;
+                dataGridView1.Rows[satir].Cells[0].Value = dr["CategoryName"].ToString();
+                dataGridView1.Rows[satir].Cells[1].Value = dr["ProductName"].ToString();
+                dataGridView1.Rows[satir].Cells[2].Value = dr["UnitPrice"].ToString();
+                dataGridView1.Rows[satir].Cells[3].Value = dr["UnitsInStock"].ToString();
+            }
+
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+       
+    }
+}
